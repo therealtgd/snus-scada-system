89c69fe25d96a294387d8683cb3e93007dd66d4e
therealtgd <vladanmikic2000@gmail.com>
2022-02-14 22:22:07 +0100

    Adds IDatabaseManager

---
+++ ScadaSystem/ScadaSystem/IDatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScadaSystem
+{
+    [ServiceContract]
+    interface IDatabaseManager
+    {
+        [OperationContract]
+        void ChangeOutputValue(); 
+        [OperationContract]
+        float GetOutputValue();
+        [OperationContract]
+        void TurnScanOn();
+        [OperationContract]
+        void TurnScanOff(); 
+        [OperationContract]
+        void AddTag();
+        [OperationContract]
+        void RemoveTag();
+    }
+}
