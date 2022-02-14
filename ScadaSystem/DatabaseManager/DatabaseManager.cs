81e31c6449615fa71771a5bec688c2b9e17568fd
therealtgd <vladanmikic2000@gmail.com>
2022-02-14 22:06:16 +0100

    Adds DatabaseManager class

---
+++ ScadaSystem/DatabaseManager/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManager
+{
+    public class DatabaseManager
+    {
+        public void ChangeOutputValue() { }
+        public float GetOutputValue() { return 0; }
+        public void TurnScanOn() { }
+        public void TurnScanOff() { }
+        public void AddTag() { }
+        public void RemoveTag() { }
+        public void Login() { }
+        public void Logout() { }
+        public void Register() { }
+    }
+}
